c90e32c3c35d667e43d4deb4c42b615e6558f18b
Ryan2429 <aldous.ryan1@gmail.com>
2019-08-18 21:30:24 -0400

    fixed url

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ displayFaceBox = (box) => {
 
   onButtonSubmit = (e) => {
     this.setState({ imageUrl: this.state.input });
-      fetch('http://localhost:3000/image', {
+      fetch('https://vast-forest-32988.herokuapp.com/image', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -126,7 +126,7 @@ displayFaceBox = (box) => {
           .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://vast-forest-32988.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
